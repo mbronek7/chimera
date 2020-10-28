a778489ed1b8668276710c11f7883640b58980c1
mbronek7 <mbronek7@gmail.com>
2020-10-28 11:52:54 +0100

    Handle 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  def not_found
+    render_error_payload(:not_found, status: :not_found)
+  end
+
+  protected
+
+  def render_error_payload(identifier, status: :bad_request)
+    render json: ErrorPayload.new(identifier, status), status: status
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  alias GET get
+  alias POST post
+  alias PUT put
+  alias PATCH patch
+  alias DELETE delete
+
+  GET "/*a", to: "application#not_found"
 end
